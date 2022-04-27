7c099baeeeeb81e6fc4c7ece4c3ba4566627c283
furkancetinalp <99509540+furkancetinalp@users.noreply.github.com>
2022-04-28 02:12:43 +0300

    Scripts to execute the contract

---
+++ scripts/1.init-contract.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+[ -z "$CONTRACT" ] && echo "Missing \$CONTRACT environment variable"
+[ -z "$OWNER" ] && echo "Missing \$OWNER environment variable"
+
+echo --------------------------------------------
+echo
+echo "cleaning up the /neardev folder"
+echo
+rm -rf ./neardev
+
+# exit on first error after this point to avoid redeploying with successful build
+set -e
+
+echo --------------------------------------------
+echo
+echo "rebuilding the contract (release build)"
+echo
+yarn build:release
+
+
+echo --------------------------------------------
+echo
+echo "redeploying the contract"
+echo
+near dev-deploy ./build/release/ncd.wasm
+
+echo --------------------------------------------
+echo run the following commands
+echo
+echo 'export CONTRACT=<dev-123-456>'
+echo 'export OWNER=<your own account>'
+echo
+echo
+
+exit 0
